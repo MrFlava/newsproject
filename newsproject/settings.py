596c4ba7cb743b88b7f8373ccdd66710863d9707
Mr.Flava <thatelitemaili33t@gmail.com>
2020-10-30 01:25:40 +0200

    Removed venv

--- newsproject/settings.py
+++ newsproject/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 import os
 import dj_database_url
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "79i4tjh)y4b&h#*@xxb38g50l7*kkx)h7d)mwz(c=642rq_31="
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
